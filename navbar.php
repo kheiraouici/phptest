ea7ffbe9ffeb06359fd87709eefd1212edfa5e35
kheira <ouicikheira770@gmail.com>
2025-07-19 16:44:26 +0200

    correction routage

--- navbar.php
+++ navbar.php
@@ -1,6 +1,6 @@
 <nav>
   <ul>
-    <li><a href="/">Home</a></li>
+    <li><a href="home.php">Home</a></li>
     <li><a href="contact.php">About</a></li>
     <li><a href="mention.php">mention</a></li>
     <li><a href="page404.php">page404</a></li>
